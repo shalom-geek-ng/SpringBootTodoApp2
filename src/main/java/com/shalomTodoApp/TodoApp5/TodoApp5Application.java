7369fcf673305b74c1306edee192f2da9ecb3a10
shalom <oluwafemishalom3@gmail.com>
2024-01-03 11:33:57 +0100

    create Todo app

---
+++ src/main/java/com/shalomTodoApp/TodoApp5/TodoApp5Application.java
@@ -0,0 +1,13 @@
+package com.shalomTodoApp.TodoApp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shalomTodoApp/TodoApp5/TodoApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalomTodoApp.TodoApp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
